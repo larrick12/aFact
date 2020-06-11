96b43569ba9101cb0eb3fc11c2c72a113d8ad291
Larrick <53548060+larrick12@users.noreply.github.com>
2020-06-11 01:02:10 +0100

    Add files via upload

---
+++ newDataAwards.dart
@@ -0,0 +1,19 @@
+class Awards {
+  String imageUrl;
+  String artists;
+  String songTitle;
+  String album;
+  String bestPopFemale;
+  String bestPopMale;
+  String songOftheyear;
+
+  Awards({
+    this.album,
+    this.artists,
+    this.bestPopFemale,
+    this.bestPopMale,
+    this.imageUrl,
+    this.songOftheyear,
+    this.songTitle,
+  });
+}
